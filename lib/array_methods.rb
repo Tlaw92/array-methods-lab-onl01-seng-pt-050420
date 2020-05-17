7b75556dbd686799e9ef9ec556f265098056d924
travis Lawson <travislawclimb@gmail.com>
2020-05-17 03:13:04 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,20 +4,21 @@ end
 
 def using_sort(array)
   array.sort
-end
+end  
 
 def using_reverse(array)
   array.reverse
-end
+end  
 
 def using_first(array)
   array.first
-end
+end  
 
 def using_last(array)
   array.last
-end
+end  
 
 def using_size(array)
-  array.size
+  array.size 
 end
+
